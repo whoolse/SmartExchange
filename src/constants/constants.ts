1ecfa0ff9f9f3f393b1d59adfb6650efa3dc1950
Yuri Nazarenko <y-nazar@yandex.ru>
2025-07-17 18:03:02 +0300

    master addresses

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -25,13 +25,13 @@ export const currencies: Record<string, Currency> = {
   SE2: { masterAddress: 'kQCXBMC_4duteYxznKizeIzhhCiR0ETTEYXIQmuURHLJ7OB0', id: 2, icon: SE2 },
   TON: { masterAddress: '', id: 0, icon: TonIcon },
   "USD₮": { masterAddress: 'EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs', id: 3, icon: USDTIcon },
-  USDe: { masterAddress: '', id: 4, icon: USDeIcon },
-  tsTON: { masterAddress: '', id: 5, icon: TsTONIcon },
-  NOT: { masterAddress: '', id: 6, icon: NotIcon },
-  STORM: { masterAddress: '', id: 7, icon: StormIcon },
-  DOGS: { masterAddress: '', id: 8, icon: DogsIcon },
-  CATI: { masterAddress: '', id: 9, icon: CatiIcon },
-  HMSTR: { masterAddress: '', id: 10, icon: HmstrIcon },
+  USDe: { masterAddress: 'EQAIb6KmdfdDR7CN1GBqVJuP25iCnLKCvBlJ07Evuu2dzP5f', id: 4, icon: USDeIcon },
+  tsTON: { masterAddress: 'EQC98_qAmNEptUtPc7W6xdHh_ZHrBUFpw5Ft_IzNU20QAJav', id: 5, icon: TsTONIcon },
+  NOT: { masterAddress: 'EQAvlWFDxGF2lXm67y4yzC17wYKD9A0guwPkMs1gOsM__NOT', id: 6, icon: NotIcon },
+  STORM: { masterAddress: 'EQBsosmcZrD6FHijA7qWGLw5wo_aH8UN435hi935jJ_STORM', id: 7, icon: StormIcon },
+  DOGS: { masterAddress: 'EQCvxJy4eG8hyHBFsZ7eePxrRsUQSFE_jpptRAYBmcG_DOGS', id: 8, icon: DogsIcon },
+  CATI: { masterAddress: 'EQD-cvR0Nz6XAyRBvbhz-abTrRC6sI5tvHvvpeQraV9UAAD7', id: 9, icon: CatiIcon },
+  HMSTR: { masterAddress: 'EQAJ8uWd7EBqsmpSWaRdf_I-8R8-XHwh3gsNKhy-UrdrPcUo', id: 10, icon: HmstrIcon },
 };
 
 export const assets = Object.keys(currencies) as Array<keyof typeof currencies>;
